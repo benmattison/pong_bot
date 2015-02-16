0867bcfdbb6725ec609357ff94a2144cff1033ac
Ben Mattison <ben.mattison@gmail.com>
2015-02-16 15:10:28 -0800

    updated to edit webcam image

--- cam_test2.py
+++ cam_test2.py
@@ -1,4 +1,4 @@
-from SimpleCV import Camera, Display
+from SimpleCV import Camera, Display, Image, Display, DrawingLayer, Color
 from time import sleep
 
 myCamera = Camera(prop_set={'width' : 320, 'height' : 240})
@@ -7,10 +7,15 @@ myDisplay = Display(resolution=(320, 240))
 
 while not myDisplay.isDone():
 	frame = myCamera.getImage()
+	myDrawingLayer = DrawingLayer((frame.width,frame.height))
+	myDrawingLayer.setFontSize(45)
 	faces = frame.findHaarFeatures('face')
 	if faces:
 		for face in faces:
 			print "Face at: " + str(face.coordinates())
+			myDrawingLayer.text("FACE",face.coordinates(),color=Color.WHITE)
+			frame.addDrawingLayer(myDrawingLayer)
+			frame.applyLayers()
 	else:
 		print "No faces detected"
 	frame.save(myDisplay)
