92d09b861f8011eeced78370dc39ffc9460205ec
Ben Mattison <ben.mattison@gmail.com>
2015-02-16 14:56:45 -0800

    fixed incorrect braces

--- cam_test1.py
+++ cam_test1.py
@@ -1,7 +1,7 @@
 from SimpleCV import Camera, Display
 from time import sleep
 
-myCamera = Camera(prop_set=('width': 320, 'height': 240))
+myCamera = Camera(prop_set={'width' : 320, 'height' : 240})
 
 myDisplay = Display(resolution=(320, 240))
 
