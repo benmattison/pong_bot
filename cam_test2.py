18956744d4a92b6d676851ee01bbd8d50f0637d4
Ben Mattison <ben.mattison@gmail.com>
2015-02-16 15:28:02 -0800

    added box around face

--- cam_test2.py
+++ cam_test2.py
@@ -14,6 +14,9 @@ while not myDisplay.isDone():
 		for face in faces:
 			print "Face at: " + str(face.coordinates())
 			coords = face.coordinates()-(70,0)
+			boxStart = face.coordinates()-(100,100)
+			boxEnd = face.coordinates()+(100,100)
+			myDrawingLayer.rectange(boxStart,boxEnd)
 			myDrawingLayer.text("I SEE YOU",coords,color=Color.WHITE)
 			frame.addDrawingLayer(myDrawingLayer)
 			frame.applyLayers()
