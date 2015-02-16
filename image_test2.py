92ecefd2494872e267260ed5b805de45d5ac977f
Ben Mattison <ben.mattison@gmail.com>
2015-02-16 14:44:33 -0800

    image file editing test

---
+++ image_test2.py
@@ -0,0 +1,21 @@
+from SimpleCV import Image, Display
+from time import sleep
+
+myDisplay = Display()
+
+raspberryImage = Image("beer-pong.jpg")
+
+myDrawingLayer = DrawingLayer((raspberryImage.width,raspberryImage.height))
+
+myDrawingLayer.rectangle((50,20),(250,60),filled=True)
+myDrawingLayer.setFontSize(45)
+myDrawingLayer.text("Jesus Loves Beer Pong!",(50,20),color=Color.WHITE)
+
+raspberryImage.addDrawingLayer(myDrawingLayer)
+raspberryImage.applyLayers()
+
+raspberryImage.save(myDisplay)
+
+while not myDisplay.isDone():
+	sleep(0.1)
+
