b3c21fd0c5f2eb675c43f71c5a6a1979fe711f04
Ben Mattison <ben.mattison@gmail.com>
2015-02-17 16:13:27 -0800

    changes to blob_test1

--- blob_test1.py
+++ blob_test1.py
@@ -6,15 +6,15 @@ myCamera = Camera(prop_set={'width' : 320, 'height' : 240})
 myDisplay = Display(resolution=(320, 240))
 
 while not myDisplay.isDone():
-	green_stuff = myCamera.getImage().colorDistance(Color.RED)
+	frame = myCamera.getImage()
 
-	green_blobs = green_stuff.findBlobs()
-	green_blobs.draw()
+	cup = frame.findBlobs(threshval=(50,240,240))
+	cup.draw()
 	#blobs are returned in order of area, smallest first
 
-	print "largest red blob at " + str(green_blobs[-1].x) + ", " + str( green_blobs[-1].y)
+	print "largest red blob at " + str(cup[-1].x) + ", " + str(cup[-1].y)
 
 
-	green_stuff.save(myDisplay)
+	frame.save(myDisplay)
 	sleep(0.1)
 
