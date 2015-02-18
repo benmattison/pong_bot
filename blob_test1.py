33af47aeb4cd41c0492396736825ab7a64d9be43
Ben Mattison <ben.mattison@gmail.com>
2015-02-17 17:37:39 -0800

    changed to drawOutline instead of rectangle

--- blob_test1.py
+++ blob_test1.py
@@ -19,7 +19,7 @@ while not myDisplay.isDone():
 	#cups = blobs.filter([b.isRectangle(0.5) for b in blobs])
 
 	for b in blobs:
-		b.drawRect()
+		b.drawOutline()
 
 
 	#blobs are returned in order of area, smallest first
