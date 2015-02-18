7ca88e3d598b24447ae23dd6651fae8e15fcd28b
Ben Mattison <ben.mattison@gmail.com>
2015-02-17 16:36:58 -0800

    attempted threshold for red

--- blob_test1.py
+++ blob_test1.py
@@ -1,19 +1,24 @@
-from SimpleCV import Camera, Display, Image, Display, DrawingLayer, Color
+from SimpleCV import Camera, Display, Image, Display, DrawingLayer, Color, ColorModel
 from time import sleep
 
 myCamera = Camera(prop_set={'width' : 320, 'height' : 240})
+#redImage = Image("red.jpg")
+#redCrop = redImage.crop(0,0,320,240)
 
 myDisplay = Display(resolution=(320, 240))
 
 while not myDisplay.isDone():
 	frame = myCamera.getImage()
+	cm = ColorModel()
+	cm.add(color.RED)
 
-	cup = frame.findBlobs()
-	cup.draw()
+	cup = cm.threshold(frame)
+
+	#cup = frame.findBlobs()
+	cup.show()
 	#blobs are returned in order of area, smallest first
+	#print(cup)
 
-	print(cup)
-
-	frame.save(myDisplay)
+	#frame.save(myDisplay)
 	sleep(5)
 
